c88ef27ee4d7402a4eefa7aaed7c66521ce63a1b
Aniket-7008 <aniketmishra969@gmail.com>
2026-03-14 14:20:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
 };
 
-// Reveal animations for different sections
-ScrollReveal().reveal(".section__header", scrollRevealOption);
-ScrollReveal().reveal(".section__description", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".gallery__grid img", {
-  ...scrollRevealOption,
-  interval: 300,
-});
+ScrollReveal().reveal(".header nav", { ...scrollRevealOption, delay: 500 });
+ScrollReveal().reveal(".section__header", { ...scrollRevealOption, interval: 200 });
+ScrollReveal().reveal(".section__description", { ...scrollRevealOption, delay: 300 });
+ScrollReveal().reveal(".portfolio__card", { ...scrollRevealOption, interval: 300 });
+ScrollReveal().reveal(".gallery__grid img", { ...scrollRevealOption, interval: 200 });
 
-// Mobile menu toggle
+// Mobile Menu
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
 });
